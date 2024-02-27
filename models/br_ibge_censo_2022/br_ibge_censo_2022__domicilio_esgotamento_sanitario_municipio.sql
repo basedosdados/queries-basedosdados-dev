4ea2415118ca85a3fc0788cb541beba0d04b285f
Arthur Gusmão <arthurgusmao@id.uff.br>
2024-02-27 19:54:17 -0300

    fix: change column names

--- models/br_ibge_censo_2022/br_ibge_censo_2022__domicilio_esgotamento_sanitario_municipio.sql
+++ models/br_ibge_censo_2022/br_ibge_censo_2022__domicilio_esgotamento_sanitario_municipio.sql
@@ -1,8 +1,7 @@
 {{ config(alias='domicilio_esgotamento_sanitario_municipio',schema='br_ibge_censo_2022') }}
-select
+select 
 safe_cast(ano as int64) ano,
-safe_cast(id_municipio as string) id_municipio,
-safe_cast(tipo_esgotamento_sanitario as string) tipo_esgotamento_sanitario,
-safe_cast(domicilios as int64) domicilios,
+safe_cast(cod_ as string) id_municipio,
+safe_cast(tipo_de_esgotamento_sanitario as string) tipo_esgotamento_sanitario,
+safe_cast(domicilios_particulares_permanentes_ocupados_unidades_ as int64) domicilios,
 from `basedosdados-dev.br_ibge_censo_2022_staging.domicilio_esgotamento_sanitario_municipio` as t
-
