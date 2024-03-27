4d74c66f0f8df5a71f5357462f83ca7a40903d10
Arthur Gusmão <arthurgusmao@id.uff.br>
2024-03-27 12:05:33 -0300

    fix: add make_valid parameter in conversion

--- models/br_ibge_censo_2022/br_ibge_censo_2022__domicilio_morador_setor_censitario.sql
+++ models/br_ibge_censo_2022/br_ibge_censo_2022__domicilio_morador_setor_censitario.sql
@@ -17,7 +17,9 @@ select
     safe_cast(cd_rgi as string) id_regiao_imediata,
     safe_cast(cd_rgint as string) id_regiao_intermediaria,
     safe_cast(cd_concurb as string) id_concentracao_urbana,
-    safe_cast(st_geogfrom(geometry) as geography) geometria,
+    safe_cast(
+        st_geogfromwkb(geometry, planar => true, make_valid => true) as geography
+    ) geometria,
     safe_cast(v0001 as int64) pessoas,
     safe_cast(v0002 as int64) domicilios,
     safe_cast(v0003 as int64) domicilios_particulares,
