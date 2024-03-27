f4f8dd03497dab819713f2d32ffb8968ae8b8068
Arthur Gusmão <arthurgusmao@id.uff.br>
2024-03-27 11:46:46 -0300

    feat: add cluster config

--- models/br_ibge_censo_2022/br_ibge_censo_2022__domicilio_morador_setor_censitario.sql
+++ models/br_ibge_censo_2022/br_ibge_censo_2022__domicilio_morador_setor_censitario.sql
@@ -1,23 +1,30 @@
-{{ config(alias='domicilio_morador_setor_censitario',schema='br_ibge_censo_2022') }}
+{{
+    config(
+        alias="domicilio_morador_setor_censitario",
+        schema="br_ibge_censo_2022",
+        cluster_by=["id_uf", "id_municipio"],
+    )
+}}
 select
-safe_cast(id_uf as string) id_uf,
-safe_cast(id_municipio as string) id_municipio,
-safe_cast(id_setor as string) id_setor,
-safe_cast(area_setor as float64) area_setor,
-safe_cast(id_distrito as string) id_distrito,
-safe_cast(id_subdistrito as string) id_subdistrito,
-safe_cast(id_microrregiao as string) id_microrregiao,
-safe_cast(id_mesorregiao as string) id_mesorregiao,
-safe_cast(id_regiao_imediata as string) id_regiao_imediata,
-safe_cast(id_regiao_intermediaria as string) id_regiao_intermediaria,
-safe_cast(id_concentracao_urbana as string) id_concentracao_urbana,
-safe_cast(geometria as geography) geometria,
-safe_cast(pessoas as int64) pessoas,
-safe_cast(domicilios as int64) domicilios,
-safe_cast(domicilios_particulares as int64) domicilios_particulares,
-safe_cast(domicilios_coletivos as int64) domicilios_coletivos,
-safe_cast(media_moradores_domicilios as float64) media_moradores_domicilios,
-safe_cast(porcentagem_domicilios_imputados as float64) porcentagem_domicilios_imputados,
-safe_cast(domcilios_particulares_ocupados as int64) domcilios_particulares_ocupados,
-from `basedosdados-dev.br_ibge_censo_2022_staging.domicilio_morador_setor_censitario` as t
-
+    safe_cast(cd_uf as string) id_uf,
+    safe_cast(cd_mun as string) id_municipio,
+    safe_cast(cd_setor as string) id_setor,
+    safe_cast(area_km2 as float64) area_setor,
+    safe_cast(cd_dist as string) id_distrito,
+    safe_cast(cd_subdist as string) id_subdistrito,
+    safe_cast(cd_micro as string) id_microrregiao,
+    safe_cast(cd_meso as string) id_mesorregiao,
+    safe_cast(cd_rgi as string) id_regiao_imediata,
+    safe_cast(cd_rgint as string) id_regiao_intermediaria,
+    safe_cast(cd_concurb as string) id_concentracao_urbana,
+    safe_cast(st_geogfrom(geometry) as geography) geometria,
+    safe_cast(v0001 as int64) pessoas,
+    safe_cast(v0002 as int64) domicilios,
+    safe_cast(v0003 as int64) domicilios_particulares,
+    safe_cast(v0004 as int64) domicilios_coletivos,
+    safe_cast(v0005 as float64) media_moradores_domicilios,
+    safe_cast(v0006 as float64) porcentagem_domicilios_imputados,
+    safe_cast(v0007 as int64) domcilios_particulares_ocupados,
+from
+    `basedosdados-dev.br_ibge_censo_2022_staging.domicilio_morador_setor_censitario`
+    as t
