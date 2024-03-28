d3becaf6ffede12cd52d2e909c83c8b5dee01b93
Arthur Gusmão <arthurgusmao@id.uff.br>
2024-03-28 17:56:25 -0300

    fix: fix column order

--- models/br_bd_diretorios_brasil/br_bd_diretorios_brasil__setor_censitario.sql
+++ models/br_bd_diretorios_brasil/br_bd_diretorios_brasil__setor_censitario.sql
@@ -19,6 +19,9 @@ with
             nm_subdist as nome_subdistrito,
             cast(null as string) id_bairro,
             cast(null as string) nome_bairro,
+            cast(null as string) sigla_uf,
+            cast(null as string) situacao_setor,
+            cast(null as string) tipo_setor,
             cd_micro as id_microrregiao,
             nm_micro as nome_microrregiao,
             cd_meso as id_mesorregiao,
@@ -29,11 +32,9 @@ with
             nm_rgint as nome_regiao_intermediaria,
             cd_concurb as id_concentracao_urbana,
             nm_concurb as nome_concentracao_urbana,
-            cast(null as string) sigla_uf,
-            cast(null as string) situacao_setor,
-            cast(null as string) tipo_setor
         from
             `basedosdados-dev.br_ibge_censo_2022_staging.domicilio_morador_setor_censitario`
+
     ),
 
     censo_2010 as (
@@ -50,13 +51,10 @@ with
             cast(null as string) id_concentracao_urbana,
             cast(null as string) nome_concentracao_urbana
         from `basedosdados-dev.br_bd_diretorios_brasil.setor_censitario`
-
     )
 
 select *
 from censo_2022
-
 union all
-
 select *
 from censo_2010
