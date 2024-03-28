dbbb73b1759da77ee562cfc2312cffcc46708ef5
Arthur Gusmão <arthurgusmao@id.uff.br>
2024-03-28 18:26:56 -0300

    feat: rm directory columns on query and schema

--- models/br_ibge_censo_2022/br_ibge_censo_2022__domicilio_morador_setor_censitario.sql
+++ models/br_ibge_censo_2022/br_ibge_censo_2022__domicilio_morador_setor_censitario.sql
@@ -8,15 +8,8 @@
 select
     safe_cast(cd_uf as string) id_uf,
     safe_cast(cd_mun as string) id_municipio,
-    safe_cast(cd_setor as string) id_setor,
+    safe_cast(cd_setor as string) id_setor_censitario,
     safe_cast(area_km2 as float64) area_setor,
-    safe_cast(cd_dist as string) id_distrito,
-    safe_cast(cd_subdist as string) id_subdistrito,
-    safe_cast(cd_micro as string) id_microrregiao,
-    safe_cast(cd_meso as string) id_mesorregiao,
-    safe_cast(cd_rgi as string) id_regiao_imediata,
-    safe_cast(cd_rgint as string) id_regiao_intermediaria,
-    safe_cast(cd_concurb as string) id_concentracao_urbana,
     safe_cast(
         st_geogfromwkb(geometry, planar => true, make_valid => true) as geography
     ) geometria,
