162fc8395a99c0da3fce6024736ac92142ba33e4
Arthur Gusmão <arthurgusmao@id.uff.br>
2024-04-16 19:59:22 -0300

    feat: add action to check bucket name

--- .github/workflows/scripts/check_sql_files.py
+++ .github/workflows/scripts/check_sql_files.py
@@ -1,28 +1,21 @@
 import os
-import json
-
-def get_changed_files():
-    changed_files = []
-    with open(os.environ['GITHUB_EVENT_PATH'], 'r') as f:
-        event = json.load(f)
-        for file in event['pull_request']['files']:
-            if file['filename'].endswith('.sql'):
-                changed_files.append(file['filename'])
-    return changed_files
 
 def check_sql_files():
-    changed_files = get_changed_files()
     found_staging = False
-    for file in changed_files:
-        with open(file, "r") as f:
-            lines = f.readlines()
-            for line in lines:
-                if "basedosdados-staging" in line:
-                    found_staging = True
-                    print(f"Found 'basedosdados-staging' in {file}")
-                    break
-            if found_staging:
-                break
+    for root, dirs, files in os.walk("."):
+        for file in files:
+            if file.endswith(".sql"):
+                with open(os.path.join(root, file), "r") as f:
+                    lines = f.readlines()
+                    for line in lines:
+                        if "basedosdados-staging" in line:
+                            found_staging = True
+                            print(f"Found 'basedosdados-staging' in {os.path.join(root, file)}")
+                            break
+                    if found_staging:
+                        break
+        if found_staging:
+            break
     
     if found_staging:
         exit(1)
