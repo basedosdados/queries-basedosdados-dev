1bc492abc4f1caee5ce974817a1590ec90eabe5b
Arthur Gusmão <arthurgusmao@id.uff.br>
2024-05-07 18:04:57 -0300

    feat: cleaning data

--- models/br_ibge_censo_2022/br_ibge_censo_2022__indigenas_indice_envelhecimento_terras_indigenas.sql
+++ models/br_ibge_censo_2022/br_ibge_censo_2022__indigenas_indice_envelhecimento_terras_indigenas.sql
@@ -6,18 +6,66 @@
 }}
 select
     safe_cast(ano as int64) ano,
-    safe_cast(terra_indigena as string) terra_indigena,
-    safe_cast(sigla_uf as string) sigla_uf,
-    safe_cast(indice_envelhecimento as float64) indice_envelhecimento,
-    safe_cast(idade_mediana as int64) idade_mediana,
-    safe_cast(razao_sexo as float64) razao_sexo,
+    safe_cast(cod_ as string) id_terra_indigena,
     safe_cast(
-        indice_envelhecimento_populacao_quilombola as float64
+        trim(
+            regexp_extract(terra_indigena_por_unidade_da_federacao, r'([^\(]+)')
+        ) as string
+    ) terra_indigena,
+    case
+        when regexp_contains(terra_indigena_por_unidade_da_federacao, r'\(\w{2}\)')
+        then
+            safe_cast(
+                trim(
+                    regexp_extract(
+                        terra_indigena_por_unidade_da_federacao, r'\((\w{2})\)'
+                    )
+                ) as string
+            )
+        else
+            safe_cast(
+                trim(
+                    split(
+                        split(terra_indigena_por_unidade_da_federacao, '(')[
+                            safe_offset(2)
+                        ],
+                        ')'
+                    )[safe_offset(0)]
+                ) as string
+            )
+    end as sigla_uf,
+    safe_cast(
+        replace(
+            indice_de_envelhecimento_da_populacao_residente_em_terras_indigenas_idosos_60_anos_ou_mais_de_idade_razao_,
+            ",",
+            "."
+        ) as float64
+    ) indice_envelhecimento,
+    safe_cast(
+        idade_mediana_da_populacao_residente_em_terras_indigenas_anos_ as int64
+    ) idade_mediana,
+    safe_cast(
+        replace(
+            razao_de_sexo_da_populacao_residente_em_terras_indigenas_razao_, ",", "."
+        ) as float64
+    ) razao_sexo,
+    safe_cast(
+        replace(
+            indice_de_envelhecimento_da_populacao_indigena_residente_em_terras_indigenas_idosos_60_anos_ou_mais_de_idade_razao_,
+            ",",
+            "."
+        ) as float64
     ) indice_envelhecimento_populacao_quilombola,
     safe_cast(
-        idade_mediana_populacao_quilombola as int64
+        idade_mediana_da_populacao_indigena_residente_em_terras_indigenas_anos_ as int64
     ) idade_mediana_populacao_quilombola,
-    safe_cast(razao_sexo_quilombola as float64) razao_sexo_quilombola,
+    safe_cast(
+        replace(
+            razao_de_sexo_da_populacao_indigena_residente_em_terras_indigenas_razao_,
+            ",",
+            "."
+        ) as float64
+    ) razao_sexo_quilombola,
 from
     `basedosdados-dev.br_ibge_censo_2022_staging.indigenas_indice_envelhecimento_terras_indigenas`
     as t
