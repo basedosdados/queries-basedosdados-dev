090ad75d2f946aa308f10a66089fa38dde196829
Arthur Gusmão <arthurgusmao@id.uff.br>
2024-05-28 19:30:38 -0300

    feat: censo 2022 alfabetizacao models v1.1

--- models/br_ibge_censo_2022/br_ibge_censo_2022__taxa_alfabetizacao_cor_raca_grupo_idade_municipio.sql
+++ models/br_ibge_censo_2022/br_ibge_censo_2022__taxa_alfabetizacao_cor_raca_grupo_idade_municipio.sql
@@ -5,11 +5,11 @@
     )
 }}
 select
-    safe_cast(id_municipio as string) id_municipio,
-    safe_cast(cor_raca as string) cor_raca,
+    safe_cast(munic_pio__c_digo_ as string) id_municipio,
+    safe_cast(cor_ou_ra_a as string) cor_raca,
     safe_cast(sexo as string) sexo,
-    safe_cast(grupo_idade as string) grupo_idade,
-    safe_cast(taxa_alfabetizacao as string) taxa_alfabetizacao,
+    safe_cast(idade as string) grupo_idade,
+    safe_cast(valor as string) taxa_alfabetizacao,
 from
     `basedosdados-dev.br_ibge_censo_2022_staging.taxa_alfabetizacao_cor_raca_grupo_idade_municipio`
     as t
