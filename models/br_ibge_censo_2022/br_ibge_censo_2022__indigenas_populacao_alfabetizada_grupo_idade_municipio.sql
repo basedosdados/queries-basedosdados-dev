707fe5264f7ca6657e0a3715ca894db8389ce4d0
Arthur Gusmão <arthurgusmao@id.uff.br>
2024-05-28 19:30:38 -0300

    feat: censo 2022 alfabetizacao models v1.1

--- models/br_ibge_censo_2022/br_ibge_censo_2022__indigenas_populacao_alfabetizada_grupo_idade_municipio.sql
+++ models/br_ibge_censo_2022/br_ibge_censo_2022__indigenas_populacao_alfabetizada_grupo_idade_municipio.sql
@@ -1,16 +1,16 @@
 {{
     config(
-        alias="populacao_alfabetizada_cor_raca_grupo_idade_municipio",
+        alias="indigenas_populacao_alfabetizada_grupo_idade_municipio",
         schema="br_ibge_censo_2022",
     )
 }}
 select
     safe_cast(munic_pio__c_digo_ as string) id_municipio,
-    safe_cast(cor_ou_ra_a as string) cor_raca,
     safe_cast(sexo as string) sexo,
     safe_cast(idade as string) grupo_idade,
     safe_cast(alfabetiza__o as string) alfabetizacao,
-    safe_cast(valor as int64) populacao,
+    safe_cast(valor as int64) populacao_indigena,
 from
-    `basedosdados-dev.br_ibge_censo_2022_staging.populacao_alfabetizada_cor_raca_grupo_idade_municipio`
+    `basedosdados-dev.br_ibge_censo_2022_staging.indigenas_populacao_alfabetizada_grupo_idade_municipio`
     as t
+where idade not like 'Total'
