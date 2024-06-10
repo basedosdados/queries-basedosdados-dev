4a6bc63b4bcc8cd784d5230ddcaa30445174cf69
uiro-bi <pisagabriel09@gmail.com>
2024-06-10 10:05:38 -0300

    add final models

--- macros/pns_macro.sql
+++ macros/pns_macro.sql
@@ -1,17 +1,42 @@
-{% macro pns_macro(column_list) %}
+{% macro pns_macro(column_list, table_id) %}
     {% set treated_columns = [] %}
 
     {% for column in column_list %}
-        {% set new_column = (
-            "CASE                               WHEN "
-            ~ column
-            ~ " = '1' THEN 'sim'                               WHEN "
-            ~ column
-            ~ " = '2' THEN 'não'                               ELSE COALESCE(NULLIF(TRIM("
-            ~ column
-            ~ "), ''), 'Não informado')                             END AS "
-            ~ column
-        ) %}
+        {% if table_id == "microdados_2013" %}
+            {% set new_column = (
+                "CASE WHEN "
+                ~ column
+                ~ " = '1' THEN 'sim' "
+                ~ "WHEN "
+                ~ column
+                ~ " = '2' THEN 'não' "
+                ~ "ELSE COALESCE(NULLIF(TRIM("
+                ~ column
+                ~ "), ''), 'Não informado') "
+                ~ "END AS "
+                ~ column
+            ) %}
+        {% elif table_id == "microdados_2019" %}
+            {% set new_column = (
+                "CASE WHEN "
+                ~ column
+                ~ " = '1' THEN 'sim' "
+                ~ "WHEN "
+                ~ column
+                ~ " = '2' THEN 'não' "
+                ~ "WHEN "
+                ~ column
+                ~ " = '9' THEN 'ignorado' "
+                ~ "WHEN "
+                ~ column
+                ~ " = '' THEN 'não aplicável' "
+                ~ "ELSE COALESCE(NULLIF(TRIM("
+                ~ column
+                ~ "), ''), 'Não informado') "
+                ~ "END AS "
+                ~ column
+            ) %}
+        {% endif %}
         {% set treated_columns = treated_columns.append(new_column) %}
     {% endfor %}
 
