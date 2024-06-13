8780f2bd9c8c3e80c34c1b51b7be02f81bb77dd3
uiro-bi <pisagabriel09@gmail.com>
2024-06-13 10:13:35 -0300

    feat: add where config, change incremental logic

--- models/br_bcb_agencia/br_bcb_agencia__agencia.sql
+++ models/br_bcb_agencia/br_bcb_agencia__agencia.sql
@@ -6,13 +6,8 @@
         partition_by={
             "field": "ano",
             "data_type": "int64",
-            "range": {"start": 2007, "end": 2024, "interval": 1},
+            "range": {"start": 2007, "end": 2025, "interval": 1},
         },
-        pre_hook="DROP ALL ROW ACCESS POLICIES ON {{ this }}",
-        post_hook=[
-            'CREATE OR REPLACE ROW ACCESS POLICY allusers_filter                     ON {{this}}                     GRANT TO ("allUsers")                     FILTER USING (DATE_DIFF(CURRENT_DATE(),DATE(CAST(ano AS INT64),CAST(mes AS INT64),1), MONTH) > 6)',
-            'CREATE OR REPLACE ROW ACCESS POLICY bdpro_filter        ON  {{this}}                     GRANT TO ("group:bd-pro@basedosdados.org", "group:sudo@basedosdados.org")                     FILTER USING (DATE_DIFF(CURRENT_DATE(),DATE(CAST(ano AS INT64),CAST(mes AS INT64),1), MONTH) <= 6)',
-        ],
     )
 }}
 
@@ -201,6 +196,10 @@ with
             ano,
             mes
         from `basedosdados-dev.br_bcb_agencia_staging.agencia` as t
+        -- os arquivos mensais possuem cabeçalhos e rodapés que variam de posição;
+        -- Este filtro remove linhas com valores intereiramente
+        -- nulos
+        where fone = '00000nan'
     )
 
 select
@@ -225,6 +224,8 @@ select
 from wrang_data
 {% if is_incremental() %}
     where
-        date(cast(ano as int64), cast(mes as int64), 1)
-        > (select max(date(cast(ano as int64), cast(mes as int64), 1)) from {{ this }})
+        date(cast(ano as int64), cast(mes as int64), 1) not in (
+            select distinct (date(cast(ano as int64), cast(mes as int64), 1))
+            from {{ this }}
+        )
 {% endif %}
