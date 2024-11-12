419f9e1b8531ef5e7ff0c4cb6d6a1c0f59a39f66
Luiz Eduardo <luiz.sinx02@gmail.com>
2024-11-12 08:23:34 -0300

    Atualizar segundo turno resultados_candidato_municipio_zona

--- models/br_tse_eleicoes/br_tse_eleicoes__resultados_candidato_municipio_zona.sql
+++ models/br_tse_eleicoes/br_tse_eleicoes__resultados_candidato_municipio_zona.sql
@@ -6,16 +6,17 @@
         partition_by={
             "field": "ano",
             "data_type": "int64",
-            "range": {"start": 1998, "end": 2022, "interval": 2},
+            "range": {"start": 1994, "end": 2024, "interval": 2},
         },
         cluster_by=["sigla_uf"],
     )
 }}
-
 select
     safe_cast(ano as int64) ano,
     safe_cast(turno as int64) turno,
+    safe_cast(id_eleicao as string) id_eleicao,
     safe_cast(tipo_eleicao as string) tipo_eleicao,
+    safe_cast(data_eleicao as date) data_eleicao,
     safe_cast(sigla_uf as string) sigla_uf,
     safe_cast(id_municipio as string) id_municipio,
     safe_cast(id_municipio_tse as string) id_municipio_tse,
@@ -28,4 +29,6 @@ select
     safe_cast(id_candidato_bd as string) id_candidato_bd,
     safe_cast(resultado as string) resultado,
     safe_cast(votos as int64) votos
-from `basedosdados-dev.br_tse_eleicoes_staging.resultados_candidato_municipio_zona` as t
+from
+    `basedosdados-staging.br_tse_eleicoes_staging.resultados_candidato_municipio_zona`
+    as t
